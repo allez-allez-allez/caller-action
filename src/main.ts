72a222f5b776f64bacc0bbfd76ea050d3786e44a
Triet Le <triet.le@plus3it.com>
2019-10-04 06:35:23 -0400

    Update to use yarn

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
-import * as core from '@actions/core';
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const token = core.getInput("github-token", { required: true });
+    const remote_action = core.getInput("remote-action", { required: true });
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const { pull_request: pr } = github.context.payload;
+    if (!pr) {
+      throw new Error("Event payload missing `pull_request`");
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const client = new github.GitHub(token);
+    core.info(
+      `Creating a new PR on the repo ${remote_action} pull request #${pr.number}`
+    );
+    client.pulls.create();
+    core.setFailed("Failed on purpose.");
   } catch (error) {
     core.setFailed(error.message);
   }
